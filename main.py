b0b9dae8ce252d66109cbfd51d20e8c1d0b53220
jpedrocrvg <137358244+jpedrocrvg@users.noreply.github.com>
2023-11-24 13:49:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def subtract(n1,n2):
 def divide(n1,n2):
   return n1/n2
 
-
 operations = {}
 
 operations["+"] = add
@@ -18,13 +17,20 @@ operations["*"] = multiply
 operations["-"] = subtract
 operations["/"] = divide
 
-num1 = int(input('What is the first number? '))
-num2 = int(input('What is the second number? '))
-
-for sign in operations:
-  print(sign)
-operation_sign = input('What is the operation that you want to do? Pick one above: ') 
-calculation_function = operations[operation_sign]
-answer = calculation_function(num1, num2)
-
-print(f"{num1} {operation_sign} {num2} = {answer}")
+def calculate():
+  should_continue = True
+  num1 = int(input('What is the first number? '))
+  while should_continue:
+    
+    num2 = int(input('What is the second number? '))
+    operation_sign = input('What is the operation that you want to do?') 
+    calculation_function = operations[operation_sign]
+    answer = calculation_function(num1, num2)
+    
+    print(f"{num1} {operation_sign} {num2} = {answer}")
+  
+    if input(f"Do you want to continue working with {answer}? Y or N ").upper() == 'Y':
+      num1 = answer
+    else:
+      should_continue = False
+      calculate()
