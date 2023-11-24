754393842c337d77a73ce034269a6473f361dc8d
jpedrocrvg <137358244+jpedrocrvg@users.noreply.github.com>
2023-11-24 14:09:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def subtract(n1,n2):
 def divide(n1,n2):
   return n1/n2
 
+
 operations = {}
 
 operations["+"] = add
@@ -17,13 +18,17 @@ operations["*"] = multiply
 operations["-"] = subtract
 operations["/"] = divide
 
+from art import logo
+  
+
 def calculate():
+  print(logo)
   should_continue = True
-  num1 = int(input('What is the first number? '))
+  num1 = float(input('What is the first number? '))
   while should_continue:
     
-    num2 = int(input('What is the second number? '))
-    operation_sign = input('What is the operation that you want to do?') 
+    num2 = float(input('What is the second number? '))
+    operation_sign = input('What is the operation that you want to do? ') 
     calculation_function = operations[operation_sign]
     answer = calculation_function(num1, num2)
     
@@ -34,3 +39,5 @@ def calculate():
     else:
       should_continue = False
       calculate()
+
+calculate()
